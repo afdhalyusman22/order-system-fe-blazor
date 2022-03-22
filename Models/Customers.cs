7fddab923c13779cb763709092799b8e6e23fa64
afdhal.yusman <afdhal.yusman22@gmail.com>
2022-03-22 17:21:33 +0700

    first commit

---
+++ Models/Customers.cs
@@ -0,0 +1,36 @@
+namespace order_system_fe_blazor.Models;
+
+public class Attributes
+{
+    public string firstname { get; set; }
+    public string lastname { get; set; }
+    public string phone { get; set; }
+    public string address { get; set; }
+    public DateTime createdAt { get; set; }
+    public DateTime updatedAt { get; set; }
+}
+
+public class Data
+{
+    public int id { get; set; }
+    public Attributes attributes { get; set; }
+}
+
+public class Pagination
+{
+    public int page { get; set; }
+    public int pageSize { get; set; }
+    public int pageCount { get; set; }
+    public int total { get; set; }
+}
+
+public class Meta
+{
+    public Pagination pagination { get; set; }
+}
+
+public class Customers
+{
+    public List<Data> data { get; set; }
+    public Meta meta { get; set; }
+}
